7e9764feecb56c849ad836aea17a25c0e648ff56
deepak <deepak@truminds.com>
2024-09-04 14:56:45 +0530

    refractor  logo

--- iRecurit_App/frontend/src/components/top-nav-bar-default.js
+++ iRecurit_App/frontend/src/components/top-nav-bar-default.js
@@ -2,15 +2,7 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./top-nav-bar-default.module.css";
-import { ReactComponent as  Icon } from "../assets/icons/symbol.svg";
-import { ReactComponent as  IconI } from "../assets/icons/vector.svg";
-import { ReactComponent as  IconR } from "../assets/icons/vector-1.svg";
-import { ReactComponent as  IconE } from "../assets/icons/vector-2.svg";
-import { ReactComponent as  IconC } from "../assets/icons/vector-3.svg";
-import { ReactComponent as  IconU } from "../assets/icons/vector-4.svg";
-import { ReactComponent as  IconR2 } from "../assets/icons/vector-5.svg";
-import { ReactComponent as  IconI2 } from "../assets/icons/vector-6.svg";
-import { ReactComponent as  IcontT } from "../assets/icons/vector-7.svg";
+import { ReactComponent as  IrecruitLogo } from "../assets/icons/iRecurit-logo.svg";
 import { ReactComponent as  Search } from "../assets/icons/iconsearch.svg";
 import { ReactComponent as  More } from "../assets/icons/vector-21.svg";
 
@@ -33,48 +25,7 @@ const TopNavBarDefault= ({ className = "" }) => {
         <div className={styles.frameParent}>
           <div className={styles.irecruitLogoSmallParent}>
             <div className={styles.irecruitLogoSmall}>
-              <Icon
-                className={styles.symbolIcon}
-                loading="lazy"
-                alt=""
-              />
-              <div className={styles.logo}>
-                <div className={styles.i}>
-                  <div className={styles.iChild} />
-                  <IconI className={styles.vectorIcon} alt=""  />
-                </div>
-                <div className={styles.recruit}>
-                  <IconR
-                    className={styles.vectorIcon1}
-                    alt=""
-                  />
-                  <IconE
-                    className={styles.vectorIcon2}
-                    alt=""
-                  />
-                  <IconC
-                    className={styles.vectorIcon3}
-                    alt=""
-                  />
-                  <IconU
-                    className={styles.vectorIcon4}
-                    alt=""
-                  />
-                  <IconR2
-                    className={styles.vectorIcon5}
-                    alt=""
-                  />
-                  <IconI2
-                    className={styles.vectorIcon6}
-                    alt=""
-                  />
-                  <IcontT
-                    className={styles.vectorIcon7}
-                    alt=""
-
-                  />
-                </div>
-              </div>
+              <IrecruitLogo/>
             </div>
             <nav className={styles.blogLinks}>
               <a className={styles.about}>About</a>
--- iRecurit_App/frontend/src/components/top-nav-bar.js
+++ iRecurit_App/frontend/src/components/top-nav-bar.js
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./top-nav-bar.module.css";
-import { ReactComponent as  Icon } from "../assets/icons/symbol.svg";
-import { ReactComponent as  IconI } from "../assets/icons/vector.svg";
-import { ReactComponent as  IconR } from "../assets/icons/vector-1.svg";
-import { ReactComponent as  IconE } from "../assets/icons/vector-2.svg";
-import { ReactComponent as  IconC } from "../assets/icons/vector-3.svg";
-import { ReactComponent as  IconU } from "../assets/icons/vector-4.svg";
-import { ReactComponent as  IconR2 } from "../assets/icons/vector-5.svg";
-import { ReactComponent as  IconI2 } from "../assets/icons/vector-6.svg";
-import { ReactComponent as  IcontT } from "../assets/icons/vector-7.svg";
+import { ReactComponent as  IrecruitLogo } from "../assets/icons/iRecurit-logo.svg";
 import { ReactComponent as  Search } from "../assets/icons/iconsearch.svg";
 import { ReactComponent as  More } from "../assets/icons/vector-21.svg";
 import { ReactComponent as  Notification } from "../assets/icons/iconnotificationnew.svg";
@@ -32,47 +24,7 @@ const TopNavBar = ({ className = "" }) => {
         <div className={styles.logoContainerParent}>
           <div className={styles.logoContainer}>
             <div className={styles.irecruitLogoSmall} onClick={()=>{navigate("/home")}}>
-              <Icon
-                className={styles.symbolIcon}
-                loading="lazy"
-                alt=""
-              />
-              <div className={styles.logo}>
-                <div className={styles.i}>
-                  <div className={styles.iChild} />
-                  <IconI className={styles.vectorIcon} alt="" />
-                </div>
-                <div className={styles.recruit}>
-                  <IconR
-                    className={styles.vectorIcon1}
-                    alt=""
-                  />
-                  <IconE
-                    className={styles.vectorIcon2}
-                    alt=""
-                  />
-                  <IconC
-                    className={styles.vectorIcon3}
-                    alt=""
-                  />
-                  <IconU
-                    className={styles.vectorIcon4}
-                    alt=""
-                  />
-                  <IconR2
-                    className={styles.vectorIcon5}
-                    alt=""
-                  />
-                  <IconI2
-                    className={styles.vectorIcon6}
-                    alt=""
-                  />
-                  <IcontT
-                    className={styles.vectorIcon7}
-                    alt=""
-                  />
-                </div>
-              </div>
+              <IrecruitLogo/>
             </div>
             <div className={styles.primaryNav}>
               <a className={styles.jobs}>Jobs</a>
