887f1a37a4f1dde8ca9ad95cda1b640263f6d33c
Dhruv Adavadkar <adavadkardhruv@gmail.com>
2024-09-09 13:55:11 +0530

    updated url

--- iRecurit_App/accounts/urls.py
+++ iRecurit_App/accounts/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('api/activate_user/<uidb64>/<token>', views.activate_user, name="activate"),
+    #path('api/activate_user/<uidb64>/<token>', views.activate_user, name="activate"),
     #path('api/test', views.test, name="test"),
     path('api/login/', login_view),  ##endpoint for login
     path('api/signup/', signup_view),
@@ -17,8 +17,8 @@ urlpatterns = [
     path('api/get_resume/<str:user_id>/', GetResume.as_view(), name='get_resume'),
     path('api/jobstatus/', UpdateJobStatus.as_view(), name='update_job_status'),
     path('index/', views.index_html, name='index_html'),
-    #re_path(r'^$', index_view, name='index'),  # Root URL
-    #re_path(r'^.*$', index_view, name='catch-all'),  # Catch-all for any other path
+    re_path(r'^$', index_view, name='index'),  # Root URL
+    re_path(r'^.*$', index_view, name='catch-all'),  # Catch-all for any other path
     path('api/upload_profile_picture/', Upload_profile_picture.as_view(), name='Upload_profile_picture'),
     path('api/get_profile_picture/<str:user_id>/',  Profile_Picture_Retrieve.as_view(), name=' Profile_Picture_Retrieve'),
     path('api/chart-data/', Chart_Data_API.as_view(), name='chart-data'),
